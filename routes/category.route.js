545b2460de776d69d8ebbb8525504de5a68467f2
tdnam99 <tdnam.17ck1@gmail.com>
2021-10-06 23:34:26 +0700

    Newfeatute: Cài đặt CRUD category

---
+++ routes/category.route.js
@@ -0,0 +1,51 @@
+const express = require('express');
+
+const categoryModel = require('../services/models/category');
+const router = express.Router();
+
+    router.get('/', async (req, res) =>{
+    console.log('aaaa');
+
+    const data = await categoryModel.findAll();
+    if (data === 0) {
+      return res.status(500).json("was row ecfect").end();
+    }
+    res.status(202).json({data});
+  });
+
+  router.get('/delete/:id', async (req, res) =>{
+    const data = req.params.id;
+
+    const row = await categoryModel.delete(data);
+    if (row === 0) {
+      return res.status(500).json("was row ecfect").end();
+    }
+    res.status(202).json({
+        message: "add category successfully"
+    });
+  });
+
+
+  router.post('/add', async (req, res) =>{
+    const data = req.body;
+
+    var a = new Date().toString();
+
+    console.log(a);
+
+    const category = {
+        Name: data.Name,
+        DateCreated: new Date(),
+        Isdeleted: 0
+    } 
+
+    const raw = await categoryModel.add(category);
+    if (raw === 0) {
+      return res.status(500).json("was row ecfect").end();
+    }
+    res.status(202).json({
+        message: "add category successfully"
+    });
+  });
+
+module.exports = router;
---
+++ services/models/category.js
@@ -0,0 +1,15 @@
+const db = require('../../utils/db');
+const tableName = 'category';
+
+module.exports = {
+    findAll()
+    {
+        return db(tableName).where('Isdeleted', 0);
+    },
+    add(category) {
+        return db(tableName).insert(category);
+    },
+    delete(id){
+        return db(tableName).where('id', id).update('Isdeleted', 1);
+    }
+}
