b821768cdb761c72af3fb105c7652b91d40172f0
tdnam99 <tdnam.17ck1@gmail.com>
2021-10-25 00:50:41 +0700

    Feature admin upto bidder -> seller  && seller -> bidder

---
+++ routes/admin.route.js
@@ -0,0 +1,58 @@
+const express = require("express");
+
+const userModel = require("../services/models/user.model");
+const router = express.Router();
+
+router.get("/bidder", async (req, res) => {
+  const data = await userModel.findByBidder();
+  if (data === 0) {
+    return res.status(500).json("was row ecfect").end();
+  }
+
+  return res.status(202).json({ data });
+});
+
+router.get("/seller", async (req, res) => {
+  const data = await userModel.findBySeller();
+  if (data === 0) {
+    return res.status(500).json("was row ecfect").end();
+  }
+
+  return res.status(202).json({ data });
+});
+
+router.post("/upto", async (req, res) => {
+  const data = req.body;
+
+  const upto = {
+    DateUpdated: new Date(),
+    Scope: 15,
+  };
+
+  const raw = await userModel.upto(data.id, upto);
+  if (raw === 0) {
+    return res.status(500).json("was row ecfect").end();
+  }
+  res.status(202).json({
+    message: "Upto bidder to seller successfully",
+  });
+});
+
+router.post("/downto", async (req, res) => {
+  const data = req.body;
+
+  const upto = {
+    DateUpdated: new Date(),
+    Scope: 5,
+  };
+
+  const raw = await userModel.downto(data.id, upto);
+  if (raw === 0) {
+    return res.status(500).json("was row ecfect").end();
+  }
+  res.status(202).json({
+    message: "Downto bidder to seller successfully",
+  });
+});
+
+module.exports = router;
--- services/models/user.model.js
+++ services/models/user.model.js
@@ -1,77 +1,87 @@
-const db = require('../../utils/db');
-const { update } = require('./image.model');
-const tableName = 'user';
+const db = require("../../utils/db");
+const { update } = require("./image.model");
+const tableName = "user";
 
 module.exports = {
+  findAll() {
+    return db(tableName).where("Isdeleted", 0);
+  },
 
-    findAll()
-    {
-        return db(tableName).where('Isdeleted', 0);
-    },
+  findByBidder() {
+    return db(tableName).where("Isdeleted", 0).andWhere("Scope", 5);
+  },
 
-    async findById(id)
-    {
-        const rows = await db(tableName).where('id',id);
-        if(rows.length === 0)
-        {
-            return null;
-        }
-        return rows[0];
-    },
+  findBySeller() {
+    return db(tableName).where("Isdeleted", 0).andWhere("Scope", 15);
+  },
 
-    async findByMail(mail)
-    {
-        const rows = await db(tableName).where('Email','like',`${mail}`);
-        if(rows.length === 0)
-        {
-            return null;
-        }
-        return rows[0];
-    },
-
-    async findByUserName(username)
-    {
-        const rows = await db(tableName).where('Username',username);
-        if(rows.length === 0)
-        {
-            return null;
-        }
-        return rows[0]; 
-    },
-    
-    add(actor){
-        return db(tableName).insert(actor);
-    },
-
-    del(id)
-    {
-        return db(tableName).where('Id',id).del();
-    },
-
-    patch(id, user_noId)
-    {
-        return db(tableName).where('Id', id).update(user_noId);
-    },
-
-    async isValidRefeshToken(id, refreshToken){
-        const rows = await db(tableName).where('Id',id).andWhere('RefreshToken',refreshToken);
-        if(rows.length === 0)
-        {
-            return false;
-        }
-        return true;
-    },
-
-    async updatePassword(id, newHashPassword) {
-        return await db(tableName).where('Id', id).update('Password', newHashPassword);
-    },
-
-    async update(user) {
-        return await db(tableName).where('Id', user.Id)
-            .update('Email', user.Email)
-            .update('Adress', user.Address)
-            .update('Birthday', user.Birthday)
-            .update('FirstName', user.Firstname)
-            .update('LastName', user.Lastname);
+  async findById(id) {
+    const rows = await db(tableName).where("id", id);
+    if (rows.length === 0) {
+      return null;
     }
-}
+    return rows[0];
+  },
+
+  async findByMail(mail) {
+    const rows = await db(tableName).where("Email", "like", `${mail}`);
+    if (rows.length === 0) {
+      return null;
+    }
+    return rows[0];
+  },
+
+  async findByUserName(username) {
+    const rows = await db(tableName).where("Username", username);
+    if (rows.length === 0) {
+      return null;
+    }
+    return rows[0];
+  },
+
+  add(actor) {
+    return db(tableName).insert(actor);
+  },
+
+  upto(id, data) {
+    return db(tableName).where("id", id).update(data);
+  },
+
+  downto(id, data) {
+    return db(tableName).where("id", id).update(data);
+  },
+
+  del(id) {
+    return db(tableName).where("Id", id).del();
+  },
+
+  patch(id, user_noId) {
+    return db(tableName).where("Id", id).update(user_noId);
+  },
+
+  async isValidRefeshToken(id, refreshToken) {
+    const rows = await db(tableName)
+      .where("Id", id)
+      .andWhere("RefreshToken", refreshToken);
+    if (rows.length === 0) {
+      return false;
+    }
+    return true;
+  },
+
+  async updatePassword(id, newHashPassword) {
+    return await db(tableName)
+      .where("Id", id)
+      .update("Password", newHashPassword);
+  },
+
+  async update(user) {
+    return await db(tableName)
+      .where("Id", user.Id)
+      .update("Email", user.Email)
+      .update("Adress", user.Address)
+      .update("Birthday", user.Birthday)
+      .update("FirstName", user.Firstname)
+      .update("LastName", user.Lastname);
+  },
+};
