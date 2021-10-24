6dcca68e42b7f57d314bb17c28c7cced173f63b2
tdnam99 <tdnam.17ck1@gmail.com>
2021-10-24 10:50:04 +0700

    bugfix category

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use('/api/auth', require('./routes/auth.route'));
 
 app.use('/api/user', require('./routes/user.router'));
 
-app.use('/api/category', auth, require('./routes/category.route'));
+app.use('/api/category', require('./routes/category.route'));
 
 // app.use('/api/login', auth, require('./routes/login.route'));
 
--- routes/category.route.js
+++ routes/category.route.js
@@ -1,51 +1,45 @@
-const express = require('express');
+const express = require("express");
 
-const categoryModel = require('../services/models/category.model');
+const categoryModel = require("../services/models/category.model");
 const router = express.Router();
 
-    router.get('/', async (req, res) =>{
-    console.log('aaaa');
+router.get("/", async (req, res) => {
+  const data = await categoryModel.findAll();
+  if (data === 0) {
+    return res.status(500).json("was row ecfect").end();
+  }
 
-    const data = await categoryModel.findAll();
-    if (data === 0) {
-      return res.status(500).json("was row ecfect").end();
-    }
-    res.status(202).json({data});
+  return res.status(202).json({ data });
+});
+
+router.get("/delete/:id", async (req, res) => {
+  const data = req.params.id;
+
+  const row = await categoryModel.delete(data);
+  if (row === 0) {
+    return res.status(500).json("was row ecfect").end();
+  }
+  res.status(202).json({
+    message: "add category successfully",
   });
+});
 
-  router.get('/delete/:id', async (req, res) =>{
-    const data = req.params.id;
+router.post("/add", async (req, res) => {
+  const data = req.body.body;
 
-    const row = await categoryModel.delete(data);
-    if (row === 0) {
-      return res.status(500).json("was row ecfect").end();
-    }
-    res.status(202).json({
-        message: "add category successfully"
-    });
+  const category = {
+    Name: data.Name,
+    DateCreated: new Date(),
+    Isdeleted: 0,
+  };
+
+  const raw = await categoryModel.add(category);
+  if (raw === 0) {
+    return res.status(500).json("was row ecfect").end();
+  }
+  res.status(202).json({
+    message: "add category successfully",
   });
-
-
-  router.post('/add', async (req, res) =>{
-    const data = req.body;
-
-    var a = new Date().toString();
-
-    console.log(a);
-
-    const category = {
-        Name: data.Name,
-        DateCreated: new Date(),
-        Isdeleted: 0
-    } 
-
-    const raw = await categoryModel.add(category);
-    if (raw === 0) {
-      return res.status(500).json("was row ecfect").end();
-    }
-    res.status(202).json({
-        message: "add category successfully"
-    });
-  });
+});
 
 module.exports = router;
