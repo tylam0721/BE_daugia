75bd3b6be3b75c019aa8bd9094cc6cae96882477
Hieu Truong <minhh.icarus@gmail.com>
2021-10-30 19:43:52 +0700

    add watch list class

---
+++ services/models/watchList.model.js
@@ -0,0 +1,20 @@
+const db = require("../../utils/db");
+const tableName = "watchlist";
+
+module.exports = {
+    findAll() {
+        return db(tableName).where("Isdeleted", 0);
+    },
+    findbyCategory(id) {
+        return db(tableName).where("Isdeleted", 0).andWhere("IdCategory", id);
+    },
+    add(product) {
+        return db(tableName).insert(product);
+    },
+    update(product, id) {
+        return db(tableName).where("id", id).update(product);
+    },
+    delete(id) {
+        return db(tableName).where("id", id).update("Isdeleted", 1);
+    },
+}
